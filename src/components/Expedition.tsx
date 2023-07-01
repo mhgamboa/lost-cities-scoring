9c48fc51095b37e96a91a3046bde1716b65f3d4d
Marcus <marcus.gamboa@gmail.com>
2023-07-01 12:11:03 -0600

    Begin scoring formula

--- src/components/Expedition.tsx
+++ src/components/Expedition.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { FaHandshake } from "react-icons/fa";
 
 type props = {
   name: string;
@@ -22,6 +23,12 @@ export default function Expedition({ name, updateScore }: props) {
     [10, false],
   ]);
 
+  const [handshakeArray, setHandShakeArray] = useState<boolean[]>([
+    false,
+    false,
+    false,
+  ]);
+
   function handleClick(amount: number, active: boolean, itemIndex: number) {
     // let totalSum = (sum - 20) x (handshakeCount + 1) + 20(only if you have 8 or more cards)
     // cardCount >=8 && (totalSum + 20)
@@ -38,6 +45,42 @@ export default function Expedition({ name, updateScore }: props) {
     });
   }
 
+  const calculate = (
+    sum: number,
+    handshakeCount: number,
+    numberCount: number
+  ) => {
+    // Count handshakes
+    // Count number of trues in scoringitems
+    return (
+      (sum - 20) * (handshakeCount + 1) +
+      (numberCount + handshakeCount >= 8 ? 20 : 0)
+    );
+  };
+
+  function handleHandshakeClick(index: number) {
+    // let totalSum = (sum - 20) x (handshakeCount + 1) + 20(only if you have 8 or more cards)
+    // cardCount >=8 && (totalSum + 20)
+
+    // setScoringItems((prevState) => {
+    //   const newState: ScoringItem[] = [...prevState];
+
+    //   active
+    //     ? setExpeditionScore((prevScore) => prevScore - amount)
+    //     : setExpeditionScore((prevScore) => prevScore + amount);
+    //   newState[itemIndex][1] = !newState[itemIndex][1];
+    //   active = newState[itemIndex][1];
+    //   return newState;
+    // });
+
+    setHandShakeArray((prevState) => {
+      const newState: boolean[] = [...prevState];
+      newState[index] = !newState[index];
+      console.log(newState);
+      return newState;
+    });
+  }
+
   useEffect(() => {
     updateScore(name, expeditionScore);
   }, [expeditionScore, name, updateScore]);
@@ -49,20 +92,34 @@ export default function Expedition({ name, updateScore }: props) {
   }, [scoringItems]);
 
   return (
-    <div className="flex flex-col items-center">
+    <div>
       <h2>{name}</h2>
 
-      {scoringItems.map((item, index) => {
-        return (
-          <div
-            onClick={() => handleClick(item[0], item[1], index)}
-            className="border-White rounded-full border-2 border-solid p-3"
-            key={`${name}-${index}`}
-          >
-            {item[0]}
-          </div>
-        );
-      })}
+      <div className="flex flex-col items-center">
+        {handshakeArray.map((boolean, index) => {
+          return (
+            <div
+              onClick={() => handleHandshakeClick(index)}
+              className="border-White rounded-full border-2 border-solid p-3"
+              key={`${name}-${index}`}
+            >
+              <FaHandshake className="text-white" />
+            </div>
+          );
+        })}
+
+        {scoringItems.map((item, index) => {
+          return (
+            <div
+              onClick={() => handleClick(item[0], item[1], index)}
+              className="border-White rounded-full border-2 border-solid p-3"
+              key={`${name}-${index}`}
+            >
+              {item[0]}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
